5965bd13943915f85c8aeb8e835f095ae0313a12
declic info <declic info@DECLICINFO-PC>
2022-03-23 15:39:50 +0100

    coucou

---
+++ Exercice Gestion Bibliothèque/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Gestion_Bibliothèque
+{
+    internal class Class1
+    {
+
+
+    }
+}
