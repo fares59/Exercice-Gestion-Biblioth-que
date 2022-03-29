9a483fee20cf310d7b97a9a83ef4d1d8273de2ec
fares59 <nuages59@hotmail.fr>
2022-03-29 10:48:09 +0200

    class Exemplaire finish

---
+++ Exercice Gestion Bibliothèque/Exemplaire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Gestion_Bibliothèque
+{
+    internal class Exemplaire
+    {
+        private string date_achat = "";
+        private string emplacement = "";
+
+
+        public DateTime  Date_achat { get => DateTime.Parse(date_achat); set => date_achat = value.ToString("yyyy-MM-dd"); }
+        public string Emplacement { get => emplacement; set => emplacement = value; }
+
+
+
+        public string ToString()
+        {
+            return "Achat  le : " + date_achat + emplacement + " emplacement : " ;
+        }
+    }
+
+   
+
+
+}
+
--- Exercice Gestion Bibliothèque/Usure.cs
+++ Exercice Gestion Bibliothèque/Usure.cs
@@ -26,3 +26,4 @@ namespace Exercice_Gestion_Bibliothèque
     }
 }
 }
+
