c17a1e6f0a957538b61a2899c65a3cdf9c8ff771
AFOUFA96 <87020583+AFOUFA96@users.noreply.github.com>
2022-03-29 08:22:03 +0200

    c

--- Exercice Gestion Bibliothèque/livre.cs
+++ Exercice Gestion Bibliothèque/livre.cs
@@ -6,20 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exercice_Gestion_Bibliothèque
 {
-    internal class livre
+    internal class Livre
     {
         private string titre = "";
         private int ISBC = 0;
+        private List<Mots_cle> mots_cle = new();
 
         public string Titre { get => titre; set => titre = value; }
         public int ISBC1 { get => ISBC; set => ISBC = value; }
+        internal List<Mots_cle> Mots_cle { get => mots_cle; set => mots_cle = value; }
 
-        public livre()
+        public Livre()
         {
           
         }
 
-        public livre(string titre, int iSBC)
+        public Livre(string titre, int iSBC)
         {
             this.titre = titre;
             this.ISBC = iSBC;
