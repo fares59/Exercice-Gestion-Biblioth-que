457b378a9063068fed2da7396f20124f4a16dc58
Jaberkh <j.khaloua@hotmail.com>
2022-03-29 09:42:10 +0200

    que trepasse si je push

--- Exercice Gestion Bibliothèque/auteur.cs
+++ Exercice Gestion Bibliothèque/auteur.cs
@@ -10,14 +10,18 @@ namespace Exercice_Gestion_Bibliothèque
     {
         private string nom = "";
         private string prenom = "";
+        private bool deleted;
+
+        List<Livre> livres = new ();
         public string Nom { get => nom; set => nom = value; }
         public string Prenom { get => prenom; set => prenom = value; }
 
+        internal List<Livre> Livres { get => livres; set => livres = value; }
         public Auteur()
         {
         }
 
-        public Auteur(string nom, string prenom)
+        public Auteur(string nom, string prenom, bool _deleted)
         {
             this.nom = nom;
             this.prenom = prenom;
@@ -30,6 +34,17 @@ namespace Exercice_Gestion_Bibliothèque
             return tostring;
         }
 
+        public void AddLivre(Livre livre)
+        {
+            if (!livres.Contains(livre))
+            {
+                livres.Add(livre);
+            }
+            if (!livre.Auteurs.Contains(this))
+            {
+                livre.Auteurs.Add(this);
+            }
+        }
 
     }
 }
