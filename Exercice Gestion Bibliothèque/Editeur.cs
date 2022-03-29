be0c8aa03cefe6886fe80ead105c5d0932f6610c
AFOUFA96 <87020583+AFOUFA96@users.noreply.github.com>
2022-03-29 09:16:07 +0200

    last commit sp2

---
+++ Exercice Gestion Bibliothèque/Editeur.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Gestion_Bibliothèque
+{
+    internal class Editeur
+    {
+        private string nom;
+        private bool deleted;
+
+        public string Nom { get => nom; set => nom = value; }
+        public bool Deleted { get => deleted; set => deleted = value; }
+        public Editeur()
+        {
+            nom = "";
+            deleted = false;
+
+        }
+        public Editeur(string _nom)
+        {
+            this.Nom = _nom;
+        }
+        public string ToString() 
+        { 
+            return "Nom d'éditeur : " + nom;
+        }
+    }
+}
